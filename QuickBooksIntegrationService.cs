dba8fc2cbd608d729ddfb04950928436211e8753
Peter Gabris <pegas123@gmail.com>
2016-11-12 22:14:07 -0500

    service contract generated by WSDL and integration service templated
    contract by: https://test.developer.intuit.com/QBWC/TroubleshootWebServiceFS/Service.asmx?wsdl

---
+++ QuickBooksIntegrationService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Services;
+
+namespace MK.QuickBooksIntegration
+{
+    [WebService(Namespace = "http://developer.intuit.com/")]
+    [WebServiceBinding(Name = "QuickBooksIntegrationService", Namespace = "http://developer.intuit.com/")]
+    public class QuickBooksIntegrationService : IQuickBooksIntegrationService
+    {
+        public string[] authenticate(string strUserName, string strPassword)
+        {
+            // TODO validate strUserName and strPassword
+            return new string[2] { new Guid().ToString(), "" };
+        }
+
+        public string clientVersion(string strVersion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string closeConnection(string ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string connectionError(string ticket, string hresult, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getLastError(string ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int receiveResponseXML(string ticket, string response, string hresult, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string sendRequestXML(string ticket, string strHCPResponse, string strCompanyFileName, string qbXMLCountry, int qbXMLMajorVers, int qbXMLMinorVers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string serverVersion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
